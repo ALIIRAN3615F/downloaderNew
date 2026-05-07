a3e59f92f06154d9b1e5b3ceac616807a37aa470
ALIIRAN3615F <ali.s.1100b@gmail.com>
2026-05-07 23:13:17 +0300

    Create SettingsModel.cs

---
+++ R34Downloader/Models/SettingsModel.cs
@@ -0,0 +1,9 @@
+namespace R34Downloader.Models
+{
+    public static class SettingsModel
+    {
+        public static bool Video { get; set; } = true;
+        public static bool Gif { get; set; } = true;
+        public static bool Images { get; set; } = true;
+    }
+}
