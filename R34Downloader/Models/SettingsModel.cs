6018887362c2c175f8910086aa135d5146045de6
ALIIRAN3615F <ali.s.1100b@gmail.com>
2026-05-08 00:01:01 +0300

    Delete R34Downloader directory

--- R34Downloader/Models/SettingsModel.cs
+++
@@ -1,9 +0,0 @@
-namespace R34Downloader.Models
-{
-    public static class SettingsModel
-    {
-        public static bool Video { get; set; } = true;
-        public static bool Gif { get; set; } = true;
-        public static bool Images { get; set; } = true;
-    }
-}
